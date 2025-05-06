9ec965aa2a0fa6fe346562e8334befcf281768b5
CJnovack <cnovack@student.fitchburgstate.edu>
2025-05-06 15:13:43 -0400

    projectiles
    projectiles done

--- Assets/Scripts/Hunter.cs
+++ Assets/Scripts/Hunter.cs
@@ -8,20 +8,39 @@ public class Hunter : MonoBehaviour
     private Rigidbody2D hunterRb;
     private GameObject player;
     public GameObject projectilePrefab;
-    public GameObject arrow;
-    public Transform arrowSpawn;
-    public float fireTime = 5.0f;
+    public Transform firePoint;
+    public float fireRate = 2.0f;
+    private float fireCoolDown;
    // private float spawnTime = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
         hunterRb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player/Werewolf");
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        transform.up = direction;
     }
 
     // Update is called once per frame
     void Update()
     {
         hunterRb.AddForce((player.transform.position - transform.position).normalized * speed);
+        fireCoolDown -= Time.deltaTime;
+        if (fireCoolDown <= 0f)
+        {
+            ShootAtPlayer();
+            fireCoolDown = fireRate;
+        }
+    }
+    void ShootAtPlayer()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        Vector2 direction = (player.transform.position - firePoint.position).normalized;
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = direction * 10f;
+        }
     }
 }
