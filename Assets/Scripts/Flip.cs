11cb74af5cba95c9498dc2e715bea5e6e4e19bb8
CJnovack <cnovack@student.fitchburgstate.edu>
2025-05-14 17:27:24 -0400

    wolf asset
    wolf imported and flipped

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        spriteRenderer.flipX = body.velocity.x < 0f;
+        
+    }
+
+}
